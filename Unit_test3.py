1d34cf4539659c6394342751ec947added347db7
Carinaxoxo <carinazj0104@gmail.com>
2017-08-18 12:15:13 +1000

    Not empty unit test

--- Unit_test3.py
+++ Unit_test3.py
@@ -1,9 +1,17 @@
 import unittest
+import word_ladder
 
-
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class TestWL(unittest.TestCase):
+    def test_samefun(self):
+        self.assertEqual(same('hide','seek'), 0)
+        self.assertEqual(same('lead','gold'), 1)
+        self.assertEqual(same('load','gold'), 2)
+        self.assertEqual(same('site','side'), 3)
+        self.assertEqual(same('hide','hide'), 4)
+    def test_buildfun(self):
+        self.assertEqual(build('.oad', ['load','coad','hide'], {'load': False, 'coad': True, 'hide': False}, []),['load'] )
+    def test_sfindfun(self):
+        self.assert(sfind('lead', ['lead','coad','hide'], {'lead': False, 'coad': True, 'hide': False}, 'load', ['lead']), True)
 
 
 if __name__ == '__main__':
